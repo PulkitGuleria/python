39b1d7f539729eaccae2e5c02891b45d8f548610
PulkitGuleria <152914029+PulkitGuleria@users.noreply.github.com>
2023-12-15 22:47:57 +0530

    Add files via upload

---
+++ 5 strings.py
@@ -0,0 +1,9 @@
+name = "Tony stark"
+print(name.upper())
+print(name.find('s'))#index start from 0 . index is position
+print(name.replace("Tony stark" , "thor"))
+print(name)
+print('m' in name)
+print(5//2)# // to remove after decimal
+print(5%2)# % remainder operator
+print(5**2) # ** power
