b3d820efd4739dcf5e87b46a14da33a3546263d3
PulkitGuleria <152914029+PulkitGuleria@users.noreply.github.com>
2023-12-15 22:40:47 +0530

    Add files via upload

---
+++ 1 lecture.py
@@ -0,0 +1,15 @@
+print("hello world")
+name = "pulkit"
+age = 18
+print("name")
+name = "guleria"
+age = 15
+is_adult = False
+print(name)
+print(age)
+print(is_adult)
+first_name = "JOhn"
+last_name = "wick"
+print(first_name + " " +last_name)
+name = input("what is your name?")
+print(name)
